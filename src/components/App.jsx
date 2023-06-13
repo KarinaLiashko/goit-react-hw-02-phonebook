9b5eb7f7480fde07f11923fe5a3af6491eae1363
KarinaLiashko <112810602+KarinaLiashko@users.noreply.github.com>
2023-06-13 22:15:13 +0200

    phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
+import React from 'react';
+
+import { Container,  PhonebookTitle, ContactsTitle, Subtitle} from './App.styled';
+import { InputSection } from './InputSection/InputSection';
+import { ContactsList } from './ContactsList/ContactsList';
+
+
+
+
+
+  // handleInputChange = event => {
+  //   // console.log(event.currentTarget.value);
+
+  //   this.setState({contacts: event.currentTarget.value});
+  // }
+  
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+    
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+    return (
+      <>
+          <Container>
+          <PhonebookTitle>Phonebook</PhonebookTitle>
+          <InputSection> </InputSection>
+
+            <ContactsTitle>Contacts</ContactsTitle>
+          <Subtitle>Find contacts by name</Subtitle>
+          
+              <ContactsList>Contacts</ContactsList>
+          </Container>
+      </>
+    );
 };
